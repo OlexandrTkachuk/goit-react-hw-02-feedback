21650866a8236ddc72925936b8360b6f740e3487
Olexandr Tkachuk <temishena@gmail.com>
2023-02-14 15:40:39 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Feedback } from './Feedback/Feedback';
-import { Container } from './App.styled';
 
 export const App = () => {
   return (
     <>
       <Feedback />
-      {/* <Phonebook /> */}
     </>
   );
 };
