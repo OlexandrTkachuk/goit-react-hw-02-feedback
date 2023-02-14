5872563f6c9179389aa264234a8ae4cd4193dbe6
Olexandr Tkachuk <temishena@gmail.com>
2023-02-14 15:38:05 +0200

    fixed 1st task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,85 +1,12 @@
 import React from 'react';
+import { Feedback } from './Feedback/Feedback';
+import { Container } from './App.styled';
 
-export class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
-  increaseGoodCount = () => {
-    this.setState(prevState => ({ good: prevState.good + 1 }));
-  };
-
-  increaseNeutralCount = () => {
-    this.setState(prevState => ({ neutral: prevState.neutral + 1 }));
-  };
-
-  increaseBadCount = () => {
-    this.setState(prevState => ({ bad: prevState.bad + 1 }));
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    const positivePercentage = Math.round((good / total) * 100);
-
-    return (
-      <div className="Container">
-        <h2 className="Title">Please leave feedback</h2>
-        <ul className="ButtonList">
-          <li className="Item">
-            <button
-              type="button"
-              className="Button Green"
-              onClick={this.increaseGoodCount}
-            >
-              Good
-            </button>
-          </li>
-
-          <li className="Item">
-            <button
-              type="button"
-              className="Button Orangered"
-              onClick={this.increaseNeutralCount}
-            >
-              Neutral
-            </button>
-          </li>
-
-          <li className="Item">
-            <button
-              type="button"
-              className="Button Red"
-              onClick={this.increaseBadCount}
-            >
-              Bad
-            </button>
-          </li>
-        </ul>
-        <h2 className="Title">Statistics</h2>
-        <ul className="StatList">
-          <li className="StatItem">
-            <p className="StatText">Good: {good}</p>
-          </li>
-          <li className="StatItem">
-            <p className="StatText">Neutral: {neutral}</p>
-          </li>
-          <li className="StatItem">
-            <p className="StatText">Bad: {bad}</p>
-          </li>
-          <li className="StatItem">
-            <p className="StatText">Total: {total}</p>
-          </li>
-
-          <li className="StatItem">
-            <p className="StatText">
-              Positive feedback: {total > 0 ? positivePercentage : '0'}%
-            </p>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+export const App = () => {
+  return (
+    <>
+      <Feedback />
+      {/* <Phonebook /> */}
+    </>
+  );
+};
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  width: 400px;
+
+  padding: 40px;
+  margin: 0 auto;
+  background-color: lightblue;
+  border-radius: 10px;
+`;
